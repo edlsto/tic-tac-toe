7fed565cc600fab79a0c5bdf6c88603813f296c6
Edward Stoner <edwardlstoner@gmail.com>
2020-08-08 16:41:02 -0600

    Rename component

---
+++ src/components/Message.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="message">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  props: ["message"],
+};
+</script>
+
+<style>
+.message {
+  margin-top: 2em;
+}
+</style>
