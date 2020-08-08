f45c9d48d0ad32653161f6da748cdca2869fab68
Edward Stoner <edwardlstoner@gmail.com>
2020-08-08 14:05:44 -0600

    Create square

---
+++ src/components/Square.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="square" v-on:click="handleSelect">
+    {{ value }}
+  </div></template
+>
+
+<script>
+export default {
+  name: "Square",
+  props: ["id", "value"],
+  methods: {
+    handleSelect: function() {
+      this.$emit("select", this.$props.id);
+    },
+  },
+};
+</script>
+
+<style>
+.square {
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 3em;
+  cursor: pointer;
+}
+</style>
