db5c28e9b5e39d29bec7e0b21f1d6af2c6b8b64a
Edward Stoner <edwardlstoner@gmail.com>
2020-08-08 16:40:29 -0600

    Add inputs for players

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="game">
+    <div v-if="player1">
+      <div>Player 1</div>
+      <div>{{ player1 }}</div>
+    </div>
+    <div v-if="player2">
+      <div>Player 2</div>
+      <div>{{ player2 }}</div>
+    </div>
+
+    <div class="player-input" v-if="!player1 || !player2">
+      <label for="input" v-if="!player1">Player 1</label>
+      <label for="input" v-else>Player 2</label>
+      <input
+        type="text"
+        v-model="input"
+        id="input"
+        v-on:keyup.enter="addPlayer"
+      />
+    </div>
+
     <div class="board">
       <Square
         v-for="(square, index) in board"
@@ -9,13 +29,13 @@
         v-on:select="selectSquare"
       />
     </div>
-    <ErrorMessage v-bind:error="this.error" />
+    <Message v-bind:message="this.message" />
   </div>
 </template>
 
 <script>
 import Square from "./Square";
-import ErrorMessage from "./ErrorMessage.vue";
+import Message from "./Message.vue";
 
 import Vue from "vue";
 
@@ -26,22 +46,26 @@ export default {
       currentPlayer: 1,
       player1: "",
       player2: "",
-      error: "",
+      input: "",
+      message: "",
+      gameOver: false,
     };
   },
   name: "Board",
   methods: {
     selectSquare: function(id) {
-      if (!this.board[parseInt(id)]) {
+      if (this.gameOver) {
+        return;
+      } else if (!this.board[parseInt(id)]) {
         Vue.set(this.board, parseInt(id), this.currentPlayer === 1 ? "X" : "O");
-        this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
-        this.error = "";
+        this.evaluateForWin(this.board);
       } else {
-        this.error = "Tile already taken!";
+        this.message = "Tile already taken!";
       }
     },
-    evaluateForWin: function(gameBoard, turn) {
-      const playerSymbol = turn % 2 === 0 ? "X" : "O";
+    evaluateForWin: function(gameBoard) {
+      const playerSymbol = this.currentPlayer === 1 ? "X" : "O";
+      console.log(playerSymbol);
       const combinations = [
         [0, 1, 2],
         [3, 4, 5],
@@ -50,16 +74,35 @@ export default {
         [1, 4, 7],
         [2, 5, 8],
       ];
-      return combinations.some((arr) => {
+      const result = combinations.some((arr) => {
         return arr
           .map((el) => gameBoard[el])
           .every((el) => el === playerSymbol);
       });
+      if (result) {
+        this.message = `${
+          this.currentPlayer === 1 ? this.player1 : this.player2
+        } won!`;
+        this.gameOver = true;
+        return;
+      } else {
+        this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
+        this.message = "";
+      }
+    },
+    addPlayer: function() {
+      if (!this.player1) {
+        this.player1 = this.input;
+        this.input = "";
+      } else {
+        this.player2 = this.input;
+        this.input = "";
+      }
     },
   },
   components: {
     Square,
-    ErrorMessage,
+    Message,
   },
 };
 </script>
@@ -74,5 +117,8 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.player-input {
+  margin-bottom: 1em;
 }
 </style>
